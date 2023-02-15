1210cb0a3c54f26844b28ddf96ba84b4e88a038c
Frank <841290355@qq.com>
2023-02-15 22:14:14 +0800

    feat: 引入路由

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "amfe-flexible"
+import router from './router'
 
 //在使用函数组件时，unplugin-vue-components 无法自动引入对应的样式，因此需要手动引入样式。
 // Toast
@@ -13,4 +14,5 @@ import 'vant/es/notify/style'
 import 'vant/es/image-preview/style'
 
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
