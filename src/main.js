12067a6c3a311fbffac249df91047199c41ec93c
Frank <841290355@qq.com>
2023-02-15 13:22:40 +0800

    feat: 引入vant组件库

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//在使用函数组件时，unplugin-vue-components 无法自动引入对应的样式，因此需要手动引入样式。
+// Toast
+import 'vant/es/toast/style'
+// Dialog
+import 'vant/es/dialog/style'
+// Notify
+import 'vant/es/notify/style'
+// ImagePreview
+import 'vant/es/image-preview/style'
+
+const app = createApp(App)
+app.mount('#app')
