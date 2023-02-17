43b2165dd404aa79e37179968a4fed8f028a4729
Frank <841290355@qq.com>
2023-02-17 14:50:31 +0800

    feat: 封装本地操作

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
 import { createStore } from "vuex"
+import { getItem, setItem } from '../utils/storage'
 const TOKEN_KEY = 'USER_INFO'
 const store = createStore({
   state: {
     //存储当前登录用户信息，包含token等数据
-    userInfo: JSON.parse(window.localStorage.getItem(TOKEN_KEY)) || {},
+    userInfo: getItem(TOKEN_KEY)
   },
   mutations: {
     setUserInfo (state, data) {
       state.userInfo = data
-      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.userInfo))
+      setItem(TOKEN_KEY, state.userInfo)
     },
   },
 })
