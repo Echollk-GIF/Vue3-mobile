f1c60272e41cde148e4b29beebe6aed284f31ba8
Frank <841290355@qq.com>
2023-02-17 13:17:03 +0800

    feat: 登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,17 +4,19 @@
     <van-form @submit="onSubmitByPwd">
       <van-cell-group inset>
         <van-field
-          name="username"
-          placeholder="请输入用户名"
+          name="userName"
+          placeholder="请输入账号"
           left-icon="manager"
           v-model="userName"
+          :rules="userFormRules.userName"
         />
         <van-field
           type="password"
-          name="password"
+          name="userPwd"
           placeholder="请输入密码"
           left-icon="lock"
           v-model="userPwd"
+          :rules="userFormRules.userPwd"
         />
       </van-cell-group>
       <div style="margin: 16px">
@@ -27,18 +29,32 @@
 <script>
 import { reactive, toRefs } from '@vue/reactivity'
 import { loginByPwd } from '../../api/user'
+import { showSuccessToast, showFailToast, showLoadingToast } from 'vant'
 
-function submitByPwd (user) {
+function useSubmit (user) {
   const onSubmitByPwd = async () => {
+    showLoadingToast({
+      message: '登录中...',
+      forbidClick: true,
+    })
     const res = await loginByPwd(user)
     if (res.data.code === 0) {
-      console.log('登录成功')
+      showSuccessToast('登录成功')
     } else {
-      console.log('用户名或密码错误')
+      showFailToast('用户名或密码错误')
     }
   }
+  // 定义表单校验规则
+  const userFormRules = {
+    userName: [{ required: true, message: "请输入账号" }],
+    userPwd: [
+      { required: true, message: "请输入密码" },
+      { pattern: /^\d{6}$/, message: "密码格式错误" },
+    ],
+  }
   return {
-    onSubmitByPwd
+    onSubmitByPwd,
+    userFormRules
   }
 }
 export default {
@@ -49,7 +65,7 @@ export default {
     })
     return {
       ...toRefs(user),
-      ...submitByPwd(user)
+      ...useSubmit(user)
     }
   }
 }
